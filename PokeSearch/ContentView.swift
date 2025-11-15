453c128e5ccebc563fa2afe022353d77672b8954
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2025-11-15 12:36:48 +0100

    Initial Commit

---
+++ PokeSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokeSearch
+//
+//  Created by Andre Veltens on 15.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokeSearch/PokeSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeSearchApp.swift
+//  PokeSearch
+//
+//  Created by Andre Veltens on 15.11.25.
+//
+
+import SwiftUI
+
+@main
+struct PokeSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

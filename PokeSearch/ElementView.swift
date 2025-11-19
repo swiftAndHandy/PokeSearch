b895b1e6324ec5a66e6730dbd82e9977b9a3887b
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2025-11-19 08:41:31 +0100

    feat: ElementView improved with ElementStyle struct

--- PokeSearch/ElementView.swift
+++ PokeSearch/ElementView.swift
@@ -9,18 +9,30 @@ import SwiftUI
 
 struct ElementModifier: ViewModifier {
     var element: Element.ElementList
+    let style: ElementStyle
+    
+    struct ElementStyle {
+        let foreground: Color
+        let background: Color
+    }
     
     func body(content: Content) -> some View {
         content
-            .foregroundStyle(getForegroundStyle(for: element))
+            .foregroundStyle(style.foreground)
     }
     
     init(for element: Element.ElementList) {
         self.element = element
+        self.style = ElementModifier.getStyle(for: element)
     }
     
-    func getForegroundStyle(for element: Element.ElementList) -> Color {
-        return Color.red
+    static func getStyle(for element: Element.ElementList) -> ElementStyle {
+        switch element {
+        case .fire:
+            return ElementStyle(foreground: .red, background: .yellow)
+        default:
+            return ElementStyle(foreground: .black, background: .white)
+        }
     }
 }
 
